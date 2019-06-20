f98150b681f51f665d12c7456734043afcc4baaa
Shantina <dinadamjanovik@gmail.com>
2019-06-20 15:57:29 +0200

    removed slashes path

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogTemplate = path.resolve("./src/templates/blog.js")
+  const blogTemplate = path.resolve("src/templates/blog.js")
   const res = await graphql(`
     query {
       allContentfulBlogPost {
@@ -30,7 +30,7 @@ module.exports.createPages = async ({ graphql, actions }) => {
   res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.slug}`,
+      path: `blog/${edge.node.slug}`,
       context: {
         slug: edge.node.slug,
       },
