bc60d713ec4d5ffd52b3016892c7b380f23f27fe
Shantina <dinadamjanovik@gmail.com>
2019-06-20 20:55:31 +0200

    added

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogTemplate = path.resolve("src/templates/Blog.js")
+  const blogTemplate = path.resolve("./src/templates/Blog.js")
   const res = await graphql(`
     query {
       allContentfulBlogPost {
@@ -17,7 +17,7 @@ module.exports.createPages = async ({ graphql, actions }) => {
   res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `blog/${edge.node.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
         slug: edge.node.slug,
       },
